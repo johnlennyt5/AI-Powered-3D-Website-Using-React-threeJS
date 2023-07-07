4282479e77b847dd5df8160f218f9f4a74209134
johnlennyt5 <johnlennyt@gmail.com>
2023-07-06 23:20:45 -0400

    added ai powered website using react

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {proxy} from 'valtio';
+
+const state = proxy({
+intro: true,
+color: '#EFBD48',
+isLogoTexture: true,
+isFullTetxure: false,
+logoDecal: './threejs.png',
+fullDecal: './threejs.png',
+});
+
+export default state;
